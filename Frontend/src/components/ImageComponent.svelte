007fe0c87b057ce1efaa3a23c2052cd5d1782a95
palmbreeze <iamfranciscoperez@gmail.com>
2023-12-05 01:28:53 +0100

    Frontend: Adapted drawio diagramEditor integration as an export. Credits to JGraph Ltd and Gaudenz Alder.

--- Frontend/src/components/ImageComponent.svelte
+++ Frontend/src/components/ImageComponent.svelte
@@ -1,13 +1,20 @@
 <script>
     import { onMount } from "svelte";
-    import { globalState } from "../stores.js";
-
+    import { DiagramEditor } from "../utils/diagramEditor";
+  
     export let imageSrc;
-</script>
-
-<div>
+  
+    onMount(() => {
+      const imgElement = document.getElementById("ErDiagram");
+      imgElement.addEventListener("click", () => {
+        DiagramEditor.editElement(imgElement, null, "sketch", null, ["dark=1", "pv=0"]);
+      });
+    });
+  </script>
+  
+  <div>
     <img id="ErDiagram" title="Click to edit image" alt="ER diagram" src={imageSrc} />
-</div>
+  </div>
 
 <style>
     #ErDiagram {
